c5d1d9bb1f8e285a6bbc9c8b11c9d2b04aabc323
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-09-11 13:13:45 -0500

    Updated OperationalEncoding comments

--- sttp/transport/operationalencoding/OperationalEncoding.go
+++ sttp/transport/operationalencoding/OperationalEncoding.go
@@ -27,10 +27,10 @@ package operationalencoding
 type OperationalEncoding uint32
 
 const (
-	// UTF-16, little endian (deprecated)
+	// UTF16LE targets little-endian 16-bit Unicode character encoding for strings (deprecated).
 	UTF16LE OperationalEncoding = 0x00000000
-	// UTF-16, big endian (deprecated)
+	// UTF16BE targets big-endian 16-bit Unicode character encoding for strings (deprecated).
 	UTF16BE OperationalEncoding = 0x00000100
-	// UTF-8
+	// UTF8 targets 8-bit variable-width Unicode character encoding for strings.
 	UTF8 OperationalEncoding = 0x00000200
 )
