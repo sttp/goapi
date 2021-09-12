d4bb5a5dd6a20e763a28041eedac174c5a1e0f0f
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-09-12 11:45:48 -0500

    Added comments to SubscriberConnector

--- sttp/transport/SubscriberConnector.go
+++ sttp/transport/SubscriberConnector.go
@@ -51,19 +51,20 @@ const ConnectSuccess int = 1
 const ConnectFailed int = 0
 const ConnectCanceled int = -1
 
-// Registers a callback to provide error messages each time
-// the subscriber fails to connect during a connection sequence.
+// RegisterErrorMessageCallback registers a callback to provide error messages each time the subscriber
+// fails to connect during a connection sequence.
 func (sc *SubscriberConnector) RegisterErrorMessageCallback(errorMessageCallback ErrorMessageCallback) {
 	sc.errorMessageCallback = errorMessageCallback
 }
 
-// Registers a callback to notify after an automatic reconnection attempt has been made.
-// This callback will be called whether the connection was successful or not, so it is
-// recommended to check the connected state of the subscriber using the IsConnected() method.
+// RegisterReconnectCallback registers a callback to notify after an automatic reconnection attempt
+// has been made. This callback will be called whether the connection was successful or not, so it
+// is recommended to check the connected state of the subscriber using the IsConnected() method.
 func (sc *SubscriberConnector) RegisterReconnectCallback(reconnectCallback ReconnectCallback) {
 	sc.reconnectCallback = reconnectCallback
 }
 
+// Connect initiates a connection sequence for a DataSubscriber for the specified SubscriptionInfo.
 func (sc *SubscriberConnector) Connect(subscriber *DataSubscriber, info SubscriptionInfo) int {
 	if sc.cancel {
 		return ConnectCanceled
