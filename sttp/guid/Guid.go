820c75e75a640a83cce7c6e95246b04f9b35aafb
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-10-06 18:09:54 -0500

    Added Equal, Compare and Components to Guid

--- sttp/guid/Guid.go
+++ sttp/guid/Guid.go
@@ -23,7 +23,9 @@
 
 package guid
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 // Guid is a standard UUID value that can handle alternate wire serialization options.
 type Guid uuid.UUID
@@ -39,6 +41,74 @@ func New() Guid {
 // IsZero determines if the Guid value is its zero value, i.e., empty.
 func (g Guid) IsZero() bool {
 	return g == Empty
+}
+
+// Equal returns true if the a and b Guid values are equal.
+func Equal(a, b Guid) bool {
+	g1 := [16]byte(a)
+	g2 := [16]byte(b)
+
+	for i := 0; i < 16; i++ {
+		if g1[i] != g2[2] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Compare returns an integer comparing two Guid values. The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
+func Compare(a, b Guid) int {
+	a1, b1, c1, d1 := a.Components()
+	a2, b2, c2, d2 := b.Components()
+
+	if a1 != a2 {
+		return result(a1, a2)
+	}
+
+	if b1 != b2 {
+		return result(uint32(b1), uint32(b2))
+	}
+
+	if c1 != c2 {
+		return result(uint32(c1), uint32(c2))
+	}
+
+	for i := 0; i < 8; i++ {
+		if d1[i] != d2[i] {
+			return result(uint32(d1[i]), uint32(d2[i]))
+		}
+	}
+
+	return 0
+}
+
+func result(left uint32, right uint32) int {
+	if left < right {
+		return -1
+	}
+
+	return 1
+}
+
+// Components gets the Guid value as its constituent components.
+func (g Guid) Components() (a uint32, b uint16, c uint16, d [8]byte) {
+
+	bytes := [16]byte(g)
+
+	a = (uint32(bytes[3]) << 24) | (uint32(bytes[2]) << 16) | (uint32(bytes[1]) << 8) | uint32(bytes[0])
+	b = (uint16(bytes[5]) << 8) | uint16(bytes[4])
+	c = (uint16(bytes[7]) << 8) | uint16(bytes[6])
+	d[0] = bytes[8]
+	d[1] = bytes[9]
+	d[2] = bytes[10]
+	d[3] = bytes[11]
+	d[4] = bytes[12]
+	d[5] = bytes[13]
+	d[6] = bytes[14]
+	d[7] = bytes[15]
+
+	return
 }
 
 // Parse decodes a Guid value from a string.
