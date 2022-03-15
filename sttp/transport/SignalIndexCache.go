8cb778b04ccf64a1573e58b7e312cdc477d62adc
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-03-15 16:35:01 -0500

    Updated comments

--- sttp/transport/SignalIndexCache.go
+++ sttp/transport/SignalIndexCache.go
@@ -33,7 +33,7 @@ import (
 )
 
 // SignalIndexCache maps 32-bit runtime IDs to 128-bit globally unique Measurement IDs. The structure
-// additionally provides reverse lookup and an extra mapping  to human-readable measurement keys.
+// additionally provides reverse lookup and an extra mapping to human-readable measurement keys.
 type SignalIndexCache struct {
 	reference      map[int32]uint32
 	signalIDList   []guid.Guid
